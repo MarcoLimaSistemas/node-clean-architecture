6bb011df40e35c218cf077d555d6e817091d7738
Marco Lima <marco.lima@clicksoft.com.br>
2021-03-14 17:10:32 -0300

    feat: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
 'use strict'
 export class SingUpController {
     handle (httpRequest: any): any {
-        return {
-            statusCode: 400,
-            body: new Error('Missing param: name')
+        if (!httpRequest.body.name) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: name')
+            }
+        }
+        if (!httpRequest.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: email')
+            }
         }
     }
 }
