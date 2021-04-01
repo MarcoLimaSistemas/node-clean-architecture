6d4db2480f25a10874a0e474c25762cce9a7ce9e
MarcoLimaSistemas <marco.lima@clicksoft.com.br>
2021-04-01 20:29:28 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 'use strict'
-
+import { MissingParamError } from './../errors/missing-param-error'
+import { badRequest } from './../helper/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SingUpController {
     handle (httpRequest: HttpRequest): HttpResponse {
         if (!httpRequest.body.name) {
-            return {
-                statusCode: 400,
-                body: new Error('Missing param: name')
-            }
+            return badRequest(new MissingParamError('name'))
         }
         if (!httpRequest.body.email) {
-            return {
-                statusCode: 400,
-                body: new Error('Missing param: email')
-            }
+            return badRequest(new MissingParamError('email'))
         }
     }
 }
