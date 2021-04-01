8c88d775fc45f0e4be392631ef10eabb5584b1f9
MarcoLimaSistemas <marco.lima@clicksoft.com.br>
2021-04-01 20:14:47 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,9 @@
 'use strict'
+
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SingUpController {
-    handle (httpRequest: any): any {
+    handle (httpRequest: HttpRequest): HttpResponse {
         if (!httpRequest.body.name) {
             return {
                 statusCode: 400,
