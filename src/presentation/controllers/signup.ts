570c0ccfdb8625423606f694dce8b555f6db90a4
MarcoLimaSistemas <marco.lima@clicksoft.com.br>
2021-04-01 20:33:25 -0300

    refactor: simplefy code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,11 +5,11 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SingUpController {
     handle (httpRequest: HttpRequest): HttpResponse {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingParamError('name'))
-        }
-        if (!httpRequest.body.email) {
-            return badRequest(new MissingParamError('email'))
+        const requiredFields = ['name', 'email']
+        for (const filed of requiredFields) {
+            if (!httpRequest.body[filed]) {
+                return badRequest(new MissingParamError(filed))
+            }
         }
     }
 }
