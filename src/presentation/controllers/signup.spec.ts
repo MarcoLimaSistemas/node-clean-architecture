17c7723a510ff0f46d4dd4a92ed564de76c66067
MarcoLimaSistemas <marco.lima@clicksoft.com.br>
2021-04-03 11:45:27 -0300

    refactor: change mocking estrategy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,14 +17,6 @@ const makeEmailValidator = (): EmailValidator => {
     return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-    class EmailValidatorStub implements EmailValidator {
-        isValid (email: string): boolean {
-            throw new Error()
-        }
-    }
-    return new EmailValidatorStub()
-}
 const makeSut = (): SutTypes => {
     const emailValidatorStub = makeEmailValidator()
     const sut = new SingUpController(emailValidatorStub)
@@ -118,8 +110,10 @@ describe('SignUp Controller', () => {
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
     test('Should return 500 if email validator throws', () => {
-        const emailValidatorStub = makeEmailValidatorWithError()
-        const sut = new SingUpController(emailValidatorStub)
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+            throw new Error()
+        })
 
         const httpRequest = {
             body: {
