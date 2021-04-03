90c734ffaf86b01e1c352a5c93fddc8fbda27a77
MarcoLimaSistemas <marco.lima@clicksoft.com.br>
2021-04-03 19:34:11 -0300

    test: ensure EmailValidatorAdapter return true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,20 @@
 import { EmailValidatorAdapter } from './email-validator'
-
+import validator from 'validator'
+jest.mock('validator', () => ({
+    isEmail (): Boolean {
+        return true
+    }
+}))
 describe('Email Validator adapter', () => {
     test('should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter()
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_email@email.com')
         expect(isValid).toBe(false)
+    })
+    test('should return treu if validator returns treu', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('invalid_email@email.com')
+        expect(isValid).toBe(true)
     })
 })
